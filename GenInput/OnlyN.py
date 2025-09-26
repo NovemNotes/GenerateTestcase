61ce46620dc5435d1270a5401ffd2868efd818a8
Nithit Sitthirat <87806999+NovemNotes@users.noreply.github.com>
2025-09-26 21:31:26 +0700

    Add files via upload

--- GenInput/OnlyN.py
+++ GenInput/OnlyN.py
@@ -1,24 +1,28 @@
 import random
 import os
-#โปรแกรมสุ่มเทสเคสที่ในเทสเคสมีเฉพาะ n เพียงตัวเดียว
-def generate_random_file(filename):
-    # สุ่มจำนวนเต็ม n 
-    n = random.randint(1, 10**6) #ลิมิตของ n
-    num = f"{n:02d}"
+
+def generate_random_file(filename, n):
     with open(filename, 'w') as file:
-        file.write(num)
-       
+        file.write(f"{n}")
     print(f"สร้างไฟล์ {filename} เสร็จสิ้น (number = {n})")
 
 def main():
-    #จำนวนเทสเคสที่ต้องการสร้างตั้งแต่ a ถึง b
     a = 1
     b = 20
-    
-    # สร้างไฟล์ตั้งแต่ a ถึง b
+    limit_cases = [1, 10**4]
+    used_numbers = set()
+
     for i in range(a, b + 1):
         filename = f"{i:02d}.in"
-        generate_random_file(filename)
+        if i-1 < len(limit_cases):
+            n = limit_cases[i-1]
+        else:
+            while True:
+                n = random.randint(1, 10000)
+                if n not in used_numbers:
+                    break
+        used_numbers.add(n)
+        generate_random_file(filename, n)
     
     print(f"สร้างไฟล์เสร็จสิ้นทั้งหมด {b - a + 1} ไฟล์")
     print(f"ไฟล์ถูกสร้างที่: {os.path.abspath(os.curdir)}")
